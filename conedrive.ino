c33bf75b9f309770f2f9cd9835f720e60044c5e8
mlatonin <erikdavidpiolo@gmail.com>
2025-04-12 05:16:36 -0700

    added serial communication!

--- conedrive.ino
+++ conedrive.ino
@@ -17,7 +17,9 @@ void rotateLeft();
 //rotate 1 second
 
 void setup() {
-  // Set pins as output
+  // Set pins as outpu
+  Serial.begin(9600);
+
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(ENA, OUTPUT);
@@ -39,11 +41,27 @@ void setup() {
 
 }
 
+void forward();
+void stopMotor();
+void backward();
+void spin();
+void rotateLeft();
+void rotateRight();
+
 void loop() {
-forward();
-delay(5000);
-backward();
-delay(5000);
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+//hi meow :3
+    switch (command) {
+      case 'f': forward();     break;
+      case 's': stopMotor();   break;
+      case 'b': backward();    break;
+      case 'r': rotateRight(); break;
+      case 'l': rotateLeft();  break;
+      case 'p': spin();        break;
+
+    }
+  }
 }
 
 void rotateLeft()
@@ -51,10 +69,10 @@ void rotateLeft()
 
 digitalWrite(IN1,LOW);
 digitalWrite(IN2,HIGH);
-analogWrite(ENA, 100); // 0-255 (PWM)
+analogWrite(ENA, 180); // 0-255 (PWM)
 digitalWrite(IN3,LOW);
 digitalWrite(IN4,HIGH);
-analogWrite(ENB, 100); // 0-255 (PWM)
+analogWrite(ENB, 180); // 0-255 (PWM)
 }
 
 void forward()
@@ -66,11 +84,11 @@ analogWrite(ENA, 255); // 0-255 (PWM)
 digitalWrite(IN3,LOW);
 digitalWrite(IN4,HIGH);
 analogWrite(ENB, 255); // 0-255 (PWM)
+
 }
 
 void backward()
 {
-
 digitalWrite(IN1,LOW);
 digitalWrite(IN2,HIGH);
 analogWrite(ENA, 255); // 0-255 (PWM)
@@ -81,11 +99,25 @@ analogWrite(ENB, 255); // 0-255 (PWM)
 
 void rotateRight()
 {
-
 digitalWrite(IN1,HIGH);
 digitalWrite(IN2,LOW);
-analogWrite(ENA, 100); // 0-255 (PWM)
+analogWrite(ENA, 180); // 0-255 (PWM)
 digitalWrite(IN3,HIGH);
 digitalWrite(IN4,LOW);
-analogWrite(ENB, 100); // 0-255 (PWM)
+analogWrite(ENB, 180; // 0-255 (PWM)
+}
+
+void stopMotor() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, 0);
+
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENB, 0);
+}
+
+void spin() {
+
+
 }
