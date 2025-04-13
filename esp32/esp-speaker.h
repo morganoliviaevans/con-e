0c59aa2bfa25ef6cbc6897d5f2ff47629d4809b3
skylotua <jade_lotus@icloud.com>
2025-04-12 23:12:38 -0700

    Combined everythinggggggg

---
+++ esp32/esp-speaker.h
@@ -0,0 +1,34 @@
+#ifndef SPEAKER_H
+#define SPEAKER_H
+
+#include <HardwareSerial.h>
+#include "DFRobotDFPlayerMini.h"
+
+// Use pins 20 and 21 with HardwareSerial to communicate with DFPlayer Mini
+const int PIN_MP3_RX = 20; // Connects to module's RX -- GPIO 44
+const int PIN_MP3_TX = 21; // Connects to module's TX -- GPIO 43
+
+class Speaker {
+public:
+  const int Sounds = 8;
+  enum Voiceline {
+    STARTUP = 1,
+    HELLO1,
+    OKAY,
+    SIREN,
+    HELLO3,
+    LOADUP,
+    BACKUP,
+    WHEET_WHEW
+  };
+
+  Speaker();
+  void setup();                // Initializes the speaker
+  void playTrack(Voiceline sound);   // Plays a specific track
+
+private:
+    HardwareSerial dfplayerSerial;  // Hardware serial for DFPlayer Mini
+    DFRobotDFPlayerMini player;     // DFPlayer object
+};
+
+#endif
